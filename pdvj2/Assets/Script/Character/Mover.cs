2d6a8204b21d22220ffaa4f412d317bbc18dffa7
Alamada Andrés <almada-andres@hotmail.com>
2024-09-11 19:46:22 -0300

    Animacion y sonido
    Animacion de personaje con flip y movimiento.
    Se agrego sonido al agarrar cofre.

--- pdvj2/Assets/Script/Character/Mover.cs
+++ pdvj2/Assets/Script/Character/Mover.cs
@@ -7,21 +7,32 @@ public class Mover : MonoBehaviour
     public float velocidadMovimiento = 5f;
 
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
+
     {
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoVertical = Input.GetAxis("Vertical");
-
 
         Vector2
  direccion = new Vector2(movimientoHorizontal, movimientoVertical);
 
         rb.velocity = direccion * velocidadMovimiento;
+
+        // Actualizar animaciones (ajusta los nombres de los parámetros según tus animaciones)
+        animator.SetFloat("Velocidad", Mathf.Abs(movimientoHorizontal));
+
+        // Invertir sprite si se mueve hacia la izquierda
+        spriteRenderer.flipX = movimientoHorizontal < 0;
     }
-}
+}   
